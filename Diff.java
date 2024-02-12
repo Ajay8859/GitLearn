6324245ed7ba55aaebfcf7711b33df11de76ac4e
Ajay Pratap Singh <ajaypratap8859@gmail.com>
2024-02-12 21:09:05 +0530

    add addition

--- Diff.java
+++ Diff.java
@@ -4,5 +4,8 @@ public class Diff {
     
     public static void main(String[] args) {
         System.out.println("Hello from diff");
+        int a=5;
+        int b=10;
+        System.out.println(a+b);
     }
 }
